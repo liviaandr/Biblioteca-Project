749e8d75cf14219602598ac5dff412f97a2062af
Lívia Andrade <livia.aandrade2006@gmail.com>
2026-09-02 03:27:07 -0300

    Atualizando as classes (antigos records)

--- src/br/com/biblioteca/models/Autor.java
+++ src/br/com/biblioteca/models/Autor.java
@@ -2,5 +2,41 @@ package br.com.biblioteca.models;
 
 import java.time.LocalDate;
 
-public record Autor(String nome, LocalDate dataNascimento, String email){
+public class Autor {
+    private String nome;
+    private int autorId;
+    private LocalDate dataNascimento;
+
+    public Autor(String nome, int autorId, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.autorId = autorId;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Autor() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return autorId;
+    }
+
+    public void setId(int autorId) {
+        this.autorId = autorId;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/br/com/biblioteca/models/Livro.java
+++ src/br/com/biblioteca/models/Livro.java
@@ -1,5 +1,81 @@
 package br.com.biblioteca.models;
 
-public record Livro(String nome, String isbn, int anoPublicacao, String editora,
-                    String genero) {
+public class Livro {
+    private String nome;
+    private int livroId;
+    private Autor autor;
+    private String isbn;
+    private int anoPublicacao;
+    private String editora;
+    private String genero;
+
+    public Livro(String nome, int livroId, Autor autor, String isbn, int anoPublicacao,
+                 String editora, String genero) {
+        this.nome = nome;
+        this.livroId = livroId;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+        this.editora = editora;
+        this.genero = genero;
+    }
+
+    public Livro() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return livroId;
+    }
+
+    public void setId(int livroId) {
+        this.livroId = livroId;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
