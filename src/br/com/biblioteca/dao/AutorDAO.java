a3b650de55c4fc4cf20e8ae9a35b141eb2b905cf
Lívia Andrade <livia.aandrade2006@gmail.com>
2026-09-02 03:35:02 -0300

    Atualizando os DAO

--- src/br/com/biblioteca/dao/AutorDAO.java
+++ src/br/com/biblioteca/dao/AutorDAO.java
@@ -1,134 +1,141 @@
 package br.com.biblioteca.dao;
 
+import br.com.biblioteca.database.DatabaseConnection;
 import br.com.biblioteca.models.Autor;
 
-public class AutorDAO implements IPersistencia<Autor>{
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AutorDAO implements IPersistencia<Autor> {
+
     @Override
-    public void inserir(Autor autor) {
-        
-        Connection con = Conexao.getConexao();
+    public void inserir(Autor objeto) {
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
 
         try {
-            stmt = con.prepareStatement("INSERT INTO AUTOR (nome) VALUES (?)");
-           
-            stmt.setString(1, curso.getNome());
+            stmt = con.prepareStatement(
+                    "INSERT INTO AUTOR (nome, data_nascimento) VALUES (?, ?)"
+            );
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setDate(2, Date.valueOf(objeto.getDataNascimento()));
 
             stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " inserido com sucesso");
+
+            System.out.println(
+                    "Autor " + objeto.getNome() + " inserido com sucesso."
+            );
 
         } catch (SQLException ex) {
             ex.printStackTrace();
 
-              throw new RuntimeException("Erro ao inserir informação no banco de dados");
+            throw new RuntimeException(
+                    "Erro ao tentar inserir informação no banco de dados."
+            );
+
         } finally {
-            Conexao.fecharConexao(con, stmt);
-
+            DatabaseConnection.closeConnection(con, stmt);
         }
-
-        
     }
 
     @Override
     public Autor consultar(String id) {
-        
-    Connection con = Conexao.getConexao();   
-    PreparedStatement stmt = null;
-       
-       ResultSet rs = null;
-       
-       
-       List<Curso> cursos = new ArrayList<Curso>();
-       
-       
-       try{
-           
-           stmt = con.prepareStatement("select id, nome from CURSO");
-           rs = stmt.executeQuery();
-           
-           while (rs.next()){
-               Curso curso =  new Curso();
-              
-               curso.setId(rs.getInt("id"));
-               curso.setNome(rs.getString("nome"));
-              
-               
-               cursos.add(curso);
-               
-           }
-           
-           
-       }catch (SQLException s){
-           s.printStackTrace();
-           
-       }
-       
-        finally {
-            Conexao.fecharConexao(con, stmt);
-
-        }
-       
-      return cursos;
-
-       
-   }
-        
-    }
-
-    @Override
-    public void alterar(String id, Autor autor) {
-        
-        Connection con = Conexao.getConexao();
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
+        ResultSet rs = null;
 
         try {
-            stmt = con.prepareStatement("UPDATE CURSO SET nome = ? where id = ? ");
-           
-            stmt.setString(1, curso.getNome());
+            stmt = con.prepareStatement(
+                    "SELECT id, nome, data_nascimento " +
+                            "FROM AUTOR WHERE id = ?"
+            );
 
-            stmt.setInt(2, curso.getId());
-            
-            
-            stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " alterado com sucesso");
+            stmt.setString(1, id);
 
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Autor autor = new Autor();
+
+                autor.setId(rs.getInt("id"));
+                autor.setNome(rs.getString("nome"));
+                autor.setDataNascimento(
+                        rs.getDate("data_nascimento").toLocalDate()
+                );
+
+                return autor;
+            }
 
         } catch (SQLException ex) {
             ex.printStackTrace();
 
-             throw new RuntimeException("Erro ao inserir informação no banco de dados");
         } finally {
-            Conexao.fecharConexao(con, stmt);
+            DatabaseConnection.closeConnection(con, stmt);
+        }
 
+        return null;
+    }
+
+    @Override
+    public void alterar(String id, Autor objeto) {
+        Connection con = DatabaseConnection.getDatabaseConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement(
+                    "UPDATE AUTOR " +
+                            "SET nome = ?, data_nascimento = ? " +
+                            "WHERE id = ?"
+            );
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setDate(2, Date.valueOf(objeto.getDataNascimento()));
+            stmt.setString(3, id);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+            throw new RuntimeException(
+                    "Erro ao alterar informação no banco de dados"
+            );
+
+        } finally {
+            DatabaseConnection.closeConnection(con, stmt);
         }
     }
 
     @Override
     public void excluir(String id) {
-        Connection con = Conexao.getConexao();
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
 
         try {
-            stmt = con.prepareStatement("DELETE from CURSO WHERE id = ?");
-           
-            stmt.setInt(1, curso.getId());
-            
-            
-            stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " excluído com sucesso");
+            stmt = con.prepareStatement(
+                    "DELETE FROM AUTOR WHERE id = ?"
+            );
 
+            stmt.setString(1, id);
+            stmt.executeUpdate();
+
+            System.out.println(
+                    "Autor " + id + " excluído com sucesso"
+            );
 
         } catch (SQLException ex) {
             ex.printStackTrace();
 
-             throw new RuntimeException("Erro ao inserir informação no banco de dados");
+            throw new RuntimeException(
+                    "Erro ao excluir informação no banco de dados"
+            );
+
         } finally {
-            Conexao.fecharConexao(con, stmt);
-
+            DatabaseConnection.closeConnection(con, stmt);
         }
-
     }
 }
--- src/br/com/biblioteca/dao/LivroDAO.java
+++ src/br/com/biblioteca/dao/LivroDAO.java
@@ -1,131 +1,163 @@
 package br.com.biblioteca.dao;
 
+import br.com.biblioteca.database.DatabaseConnection;
+import br.com.biblioteca.models.Autor;
 import br.com.biblioteca.models.Livro;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LivroDAO implements IPersistencia<Livro> {
 
     @Override
     public void inserir(Livro objeto) {
-
-    Connection con = Conexao.getConexao();
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
 
         try {
-            stmt = con.prepareStatement("INSERT INTO CURSO (nome) VALUES (?)");
-           
-            stmt.setString(1, curso.getNome());
+            stmt = con.prepareStatement(
+                    "INSERT INTO LIVRO " +
+                            "(nome, autor_id, isbn, ano_publicacao, editora, genero) " +
+                            "VALUES (?, ?, ?, ?, ?, ?)"
+            );
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setInt(2, objeto.getAutor().getId());
+            stmt.setString(3, objeto.getIsbn());
+            stmt.setInt(4, objeto.getAnoPublicacao());
+            stmt.setString(5, objeto.getEditora());
+            stmt.setString(6, objeto.getGenero());
 
             stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " inserido com sucesso");
+
+            System.out.println(
+                    "Livro " + objeto.getNome() + " inserido com sucesso."
+            );
 
         } catch (SQLException ex) {
             ex.printStackTrace();
+            throw new RuntimeException(
+                    "Erro ao tentar inserir informação no banco de dados."
+            );
 
-              throw new RuntimeException("Erro ao inserir informação no banco de dados");
         } finally {
-            Conexao.fecharConexao(con, stmt);
-
+            DatabaseConnection.closeConnection(con, stmt);
         }
-        
     }
 
     @Override
     public Livro consultar(String id) {
-        
-      Connection con = Conexao.getConexao();
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
-       
-       ResultSet rs = null;
-       
-       
-       List<Curso> cursos = new ArrayList<Curso>();
-       
-       
-       try{
-           
-           stmt = con.prepareStatement("select id, nome from CURSO");
-           rs = stmt.executeQuery();
-           
-           while (rs.next()){
-               Curso curso =  new Curso();
-              
-               curso.setId(rs.getInt("id"));
-               curso.setNome(rs.getString("nome"));
-              
-               
-               cursos.add(curso);
-               
-           }
-           
-           
-       }catch (SQLException s){
-           s.printStackTrace();
-           
-       }
-       
-        finally {
-            Conexao.fecharConexao(con, stmt);
-
-        }
-       
-      return cursos;
-
-       
-   }
-    }
-
-    @Override
-    public void alterar(String id, Livro livro) {
-       Connection con = Conexao.getConexao();
-        PreparedStatement stmt = null;
+        ResultSet rs = null;
 
         try {
-            stmt = con.prepareStatement("UPDATE CURSO SET nome = ? where id = ? ");
-           
-            stmt.setString(1, curso.getNome());
+            stmt = con.prepareStatement(
+                    "SELECT livro_id, nome, autor_id, isbn, ano_publicacao, editora, genero " +
+                            "FROM LIVRO WHERE livro_id = ?"
+            );
 
-            stmt.setInt(2, curso.getId());
-            
-            
-            stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " alterado com sucesso");
+            stmt.setString(1, id);
 
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int livroId = rs.getInt("livro_id");
+                String nome = rs.getString("nome");
+                int autorId = rs.getInt("autor_id");
+                String isbn = rs.getString("isbn");
+                int anoPublicacao = rs.getInt("ano_publicacao");
+                String editora = rs.getString("editora");
+                String genero = rs.getString("genero");
+
+                AutorDAO autorDAO = new AutorDAO();
+                Autor autor = autorDAO.consultar(String.valueOf(autorId));
+
+                Livro livro = new Livro();
+
+                livro.setId(livroId);
+                livro.setNome(nome);
+                livro.setAutor(autor);
+                livro.setIsbn(isbn);
+                livro.setAnoPublicacao(anoPublicacao);
+                livro.setEditora(editora);
+                livro.setGenero(genero);
+
+                return livro;
+            }
 
         } catch (SQLException ex) {
             ex.printStackTrace();
 
-             throw new RuntimeException("Erro ao inserir informação no banco de dados");
         } finally {
-            Conexao.fecharConexao(con, stmt);
+            DatabaseConnection.closeConnection(con, stmt);
+        }
 
+        return null;
+    }
+
+    @Override
+    public void alterar(String id, Livro objeto) {
+        Connection con = DatabaseConnection.getDatabaseConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement(
+                    "UPDATE LIVRO SET nome = ?, autor_id = ?, isbn = ?, " +
+                            "ano_publicacao = ?, editora = ?, genero = ? " +
+                            "WHERE livro_id = ?"
+            );
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setInt(2, objeto.getAutor().getId());
+            stmt.setString(3, objeto.getIsbn());
+            stmt.setInt(4, objeto.getAnoPublicacao());
+            stmt.setString(5, objeto.getEditora());
+            stmt.setString(6, objeto.getGenero());
+            stmt.setString(7, id);
+
+            stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+            throw new RuntimeException(
+                    "Erro ao alterar informação no banco de dados"
+            );
+
+        } finally {
+            DatabaseConnection.closeConnection(con, stmt);
         }
     }
 
     @Override
     public void excluir(String id) {
-        Connection con = Conexao.getConexao();
+        Connection con = DatabaseConnection.getDatabaseConnection();
         PreparedStatement stmt = null;
 
         try {
-            stmt = con.prepareStatement("DELETE from CURSO WHERE id = ?");
-           
-            stmt.setInt(1, curso.getId());
-            
-            
-            stmt.executeUpdate();
-            
-            System.out.println("Curso " + curso.getNome() + " excluído com sucesso");
+            stmt = con.prepareStatement(
+                    "DELETE FROM LIVRO WHERE livro_id = ?"
+            );
 
+            stmt.setString(1, id);
+            stmt.executeUpdate();
+
+            System.out.println(
+                    "Livro " + id + " excluído com sucesso"
+            );
 
         } catch (SQLException ex) {
             ex.printStackTrace();
 
-             throw new RuntimeException("Erro ao inserir informação no banco de dados");
-        } finally {
-            Conexao.fecharConexao(con, stmt);
+            throw new RuntimeException(
+                    "Erro ao excluir informação no banco de dados"
+            );
 
+        } finally {
+            DatabaseConnection.closeConnection(con, stmt);
         }
     }
 }
