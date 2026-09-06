33d20290e79e7249d5c83cb0994e951dcd2cbebe
Lana Andrade <lanaandradee20@gmail.com>
2026-09-05 22:00:50 -0300

    Adicionando telas MDI da biblioteca

---
+++ src/br/com/biblioteca/view/TelaLivroMDI.java
@@ -0,0 +1,16 @@
+package biblioteca;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import br.com.biblioteca.dao.AutorDAO;
+import br.com.biblioteca.dao.LivroDAO;
+import br.com.biblioteca.models.Autor;
+import br.com.biblioteca.models.Livro;
+
+
+public class TelaLivroMDI extends JInternalFrame implements ActionListener {
+private
+}
---
+++ src/br/com/biblioteca/view/TelaPrincipalMDI.java
@@ -0,0 +1,45 @@
+package biblioteca;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaPrincipalMDI extends JFrame implements ActionListener {
+    private JDesktopPane desktopPane;
+    private JMenuItem itemGerenciar;
+
+    public TelaPrincipalMDI(){
+        setTitle("Sistema de Biblioteca - MDI"):
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        desktopPane = new JDesktopPane();
+        add(desktopPane, BorderLayout.CENTER);
+
+        JMenuBar = new JMenuBar();
+        JMenuLivros = new JMenu("Livros");
+        JMenuItem itemGerenciar = new  JMenuItem("Gerenciar Livros");
+
+        itemGerenciar.addActionListener (this);
+
+        menuLivros.add(itemGerenciar);
+        menuBar.add(menuLivros);
+        setJMenuBar(menuBar);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == itemGerenciar) {
+            TelaLivroMDI telaLivro = new TelaLivroMDI();
+            desktopPane.add(telaLivro);
+            telaLivro.setVisible(true);
+            }
+        }
+
+public static void main (String[] args){
+        TelaPrincipalMDI tela = new TelaPrincipalMDI();
+        tela.setVisible(true);
+    }
+}
