f6cbb7409e6a9e5787a6de5ac60ca4946f6e91be
Suellen Araújo <contatosusuaver@gmail.com>
2026-09-05 22:32:42 -0300

    Update sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,11 +1,3 @@
--- Banco de dados da Biblioteca
--- PostgreSQL
--- Schema alinhado aos modelos Autor.java e Livro.java e aos DAOs atuais.
-
-DROP VIEW IF EXISTS vw_livros;
-DROP TABLE IF EXISTS livro;
-DROP TABLE IF EXISTS autor;
-
 CREATE TABLE autor (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -43,9 +35,4 @@ SELECT
 FROM livro l
 INNER JOIN autor a ON a.id = l.autor_id;
 
--- Dados de exemplo (opcionais)
--- INSERT INTO autor (nome, data_nascimento)
--- VALUES ('Machado de Assis', '1839-06-21');
 
--- INSERT INTO livro (nome, autor_id, isbn, ano_publicacao, editora, genero)
--- VALUES ('Dom Casmurro', 1, '9780000000000', 1899, 'Exemplo Editora', 'Romance');
